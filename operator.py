a1135cb5fb209672f50f63bfc5446d785a9c933d
khushiLavadiya303 <khushilavadiya8@gmail.com>
2026-01-27 09:14:57 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,40 @@
+#1-arithmetic operator
+"""a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)
+#2-assigment operator
+x=35
+print(x) 
+#3-unary minus
+a=3
+print(-a)
+#4-relational operator
+a=15
+b=20
+print(a>b)
+print(a==b)
+#5-logical operators
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x) 
+#6-boolean 
+is_pass=True
+print(is_pass) 
+#7- bitwise operator
+a=5
+b=3
+print(a and b)
+print(a/b) 
+#8-membership operator
+numbers=[1,2,3,4]
+print(2 in numbers)
+print(5 not in numbers) """
+#9-identity operator
+x=10
+y=10
+print(x is y)
+print(x is not y)
