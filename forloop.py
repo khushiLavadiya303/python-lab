44d803a579ea6dc42821d9d077431c0316581d9c
khushiLavadiya303 <khushilavadiya8@gmail.com>
2026-02-24 08:03:25 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,34 @@
+"""#1.print numbers from 1 to 10
+for i in range(1,11):
+    print(i)
+#2.print numbers from 1 to 20 
+for i in range(1,21):
+    print(i)
+#1.print even numbers from 1 to 15  
+for i in range(1,16):
+    if i%2==0:
+        print(i)
+#1.print odd numbers from 1 to 15   
+for i in range(1,16):
+    if i%2!=0:
+        print(i)         
+#print table of 5
+for i in range(1,11):
+    print("5x",i,"=",5*i)
+#print characters of string
+name="hello"
+for letter in name:
+    print(letter)  
+#sum of numbers from 1 to 5
+total=0
+for i in range(1,6):
+    total=total+i
+print("sum is:  ",total) 
+#print list elements    
+numbers=[10,20,30,40]
+for n in numbers: 
+    print(n) """
+#print a message three times
+for i in range(3):
+    print("hey")
+
