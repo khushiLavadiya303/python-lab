d9233d43df3267a812dafe7917fe576c23360471
khushiLavadiya303 <khushilavadiya8@gmail.com>
2026-03-31 08:44:04 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,49 @@
+'''i=0
+while i<=10:
+    print(i)
+    i+=2'''
+'''i=1
+while i<=10:
+    print(i)
+    i+=2'''
+
+'''password=""
+while password!='atmiya':
+     password=input("enter the password:")
+     print("access grated")'''
+'''n=int(input("enter a value->"))
+total=0
+i=0
+while i<=n:
+    total+=i
+    i+=1
+
+    print('sum:',total)'''
+'''name=input('enter name')
+score=input('enter score')
+print(f"{name} scored{score}% in the exam.")'''
+
+'''n=int(input('enter a value->'))
+fectorial=1
+i=1
+while i<=n:
+    fectorial+=i
+    i+=1
+
+    print("fectorial",fectorial)'''
+'''password=''
+attempts=0
+max_attempts=3
+while password!='atmiya' and attempts<max_attempts:
+  password=input("enter the password")
+  attempts+=1
+if password=='atmiya':
+    print("access grated")
+else:
+    print("access denied! you have exetend the maximum number of attempt")'''
+n=int(input("enter the number"))
+for i in range(1,11):
+    print(f'{n}*{i}={n*i}')
+
+    
+    
