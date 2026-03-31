311940dfdd3e8bb024a351eb600b0f3e72357917
khushiLavadiya303 <khushilavadiya8@gmail.com>
2026-03-31 08:43:31 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,35 @@
+'''s=list(range(0,10,2))
+print(s)'''
+'''r=list(range(10,101,10))
+result=sum(r)
+print(result)
+b=bytes([66,67,68])
+print(b)
+print(b[0])
+a=33
+b=77
+print(a==b)
+print(a!=b)'''
+'''l=[23,45,66,45]
+print(l)
+print(l[1:3])
+del l[2]
+print(l)
+a1=frozenset([1,2,3,4,5,6])
+b1=frozenset([3,4,5,6,7])
+print(a1&b1)
+print(a1|b1)'''
+'''student={'name':'mahesh',
+   'age':47,
+   'cource':'BCA'}
+    
+print(student)
+print(student['name'])
+print(student['age'])
+del student['age']
+print(student)'''
+l=[11,12,13,14]
+print(l)
+del l[1]
+print(l)
+print(-1)
