7cc4c680e662910ee5b38d0822dd2456473f57e3
khushiLavadiya303 <khushilavadiya8@gmail.com>
2026-03-31 08:44:23 +0530

    Add files via upload

---
+++ file3.py
@@ -0,0 +1,32 @@
+ '''for i in range(21):
+    if i % 2==0:
+        pass
+    else:
+        print(i,end=',')
+
+for i in range(31):
+    if i % 5==0:
+        pass
+    else:
+        print(i,end=',')'''
+
+'''DOB=int (input('enter date of birth'))
+assert DOB>0,'date of birth must be positive'
+print('date of birth is',DOB)'''
+
+'''numbers=[3,-1,4,-2,5,-3]
+for n in numbers:
+    if n<0:
+        continue
+    print(n,end=',')'''
+
+str1=input('enter string')
+str2=str1[ : :-1]
+if str1==str2:
+    print('it is palindrom')
+else:
+    print("it is not palindrom"
+
+a1={1,2,3,4,5,6,7}
+a2={2,5,8,9}
+          print(a1 & a2)
