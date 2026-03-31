79eac01d615dd53a02985c79debf845951b012c5
khushiLavadiya303 <khushilavadiya8@gmail.com>
2026-03-31 08:45:43 +0530

    Add files via upload

---
+++ file7.py
@@ -0,0 +1,14 @@
+'''for i in range(1,6):
+    for j in range(i):
+        print(i,end=" ")
+    print()'''
+'''rows=int(input('enter the numbers of rows'))
+for i in range (1,rows+1):
+    print(" ",end=" ")
+for k in range(2*i-1):
+    print('*',end=" ")
+print()'''
+
+import array
+s=array.array('i',(1,2,3,4,5))
+print(s)
