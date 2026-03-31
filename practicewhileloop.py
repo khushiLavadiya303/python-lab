2b6b37ec3b1277e9b6028ae4accca9ff64ac1262
khushiLavadiya303 <khushilavadiya8@gmail.com>
2026-03-31 08:41:47 +0530

    Add files via upload

---
+++ practicewhileloop.py
@@ -0,0 +1,20 @@
+#print numbers from 1 to 5
+"""i=1
+while i<=5:
+    print(i)
+    i=i+1  
+#sum of numbers take user input
+number=int(input("enter number: "))
+i=1
+s=0
+while i<=number:
+    s=s+i
+    i=i+1
+print("sum= ",s)   """
+#print odd number between 1 and 20
+i=1
+while i<=20:
+        if i%2!=0:
+          print(i)
+i=i+1
+     
